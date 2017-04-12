ea937c6912d8eca3800516107a902dcf1dc8b9d7
unknown <kpomservices@gmail.com>
2017-04-12 19:39:56 +0530

    flies

---
+++ loadtemplate.php
@@ -0,0 +1,14 @@
+<?php 
+	include "library/config.php";
+
+	$sel_Qry = "select canvas_json from template where template_id = " . $_GET['id'];
+	$runQry = mysql_query($sel_Qry);
+
+  	while($row = mysql_fetch_array($runQry))
+	{
+		$templatespath = $row['canvas_json'];
+		$json = file_get_contents($templatespath);
+		echo $json;
+	}
+   
+?>
